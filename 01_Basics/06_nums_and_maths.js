b48b1a3ccef30f77d631f82144ae7ebc557181b4
yashashreeyadav <45139675+yashashreeyadav@users.noreply.github.com>
2024-07-15 18:19:01 +0000

    Day 5

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,43 @@
+const score =400
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(3));
+
+const otherNumber =11122.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MIN_VALUE, Number.MAX_VALUE);
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.round(5.2));
+
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.2));
+
+// console.log(Math.min(23, 0, -6, 5));
+
+// console.log(Math.max(23, 0, -6, 5))
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+
+const min =10
+const max =20
+
+
+console.log(Math.floor(Math.random()*(max-min +1))+ min);
